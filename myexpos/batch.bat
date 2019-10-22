4b57f2170ad853f74e3da4242203ccb641da607f
mygithub9760 <39545240+mygithub9760@users.noreply.github.com>
2019-10-22 23:35:26 +0530

    Add files via upload

---
+++ myexpos/batch.bat
@@ -0,0 +1,70 @@
+load --init /home/kenil/expos/myexpos/expl/samples/login.xsm
+load --shell /home/kenil/expos/myexpos/expl/samples/shell.xsm
+load --os /home/kenil/expos/myexpos/spl/spl_progs/os_startup.xsm
+load --idle /home/kenil/expos/myexpos/expl/samples/idle.xsm
+load --exhandler /home/kenil/expos/myexpos/spl/spl_progs/exhandler.xsm
+load --int=timer /home/kenil/expos/myexpos/spl/spl_progs/sample_timer.xsm
+load --int=disk /home/kenil/expos/myexpos/spl/spl_progs/sample_intdisk.xsm
+load --library /home/kenil/expos/myexpos/expl/library.lib
+load --int=console /home/kenil/expos/myexpos/spl/spl_progs/sample_intconsole.xsm
+load --int=4 /home/kenil/expos/myexpos/spl/spl_progs/sample_int4.xsm
+load --int=5 /home/kenil/expos/myexpos/spl/spl_progs/sample_int5.xsm
+load --int=6 /home/kenil/expos/myexpos/spl/spl_progs/sample_int6.xsm
+load --int=7 /home/kenil/expos/myexpos/spl/spl_progs/sample_int7.xsm
+load --int=8 /home/kenil/expos/myexpos/spl/spl_progs/sample_int8.xsm
+load --int=9 /home/kenil/expos/myexpos/spl/spl_progs/sample_int9.xsm
+load --int=10 /home/kenil/expos/myexpos/spl/spl_progs/sample_int10.xsm
+load --int=11 /home/kenil/expos/myexpos/spl/spl_progs/sample_int11.xsm
+load --int=12 /home/kenil/expos/myexpos/spl/spl_progs/sample_int12.xsm
+load --int=13 /home/kenil/expos/myexpos/spl/spl_progs/sample_int13.xsm
+load --int=14 /home/kenil/expos/myexpos/spl/spl_progs/sample_int14.xsm
+load --int=15 /home/kenil/expos/myexpos/spl/spl_progs/sample_int15.xsm
+load --int=16 /home/kenil/expos/myexpos/spl/spl_progs/sample_int16.xsm
+load --int=17 /home/kenil/expos/myexpos/spl/spl_progs/sample_int17.xsm
+load --module 0 /home/kenil/expos/myexpos/spl/spl_progs/MOD_0.xsm
+load --module 1 /home/kenil/expos/myexpos/spl/spl_progs/MOD_1.xsm
+load --module 2 /home/kenil/expos/myexpos/spl/spl_progs/MOD_2.xsm
+load --module 3 /home/kenil/expos/myexpos/spl/spl_progs/MOD_3.xsm
+load --module 4 /home/kenil/expos/myexpos/spl/spl_progs/MOD_4.xsm
+load --module 5 /home/kenil/expos/myexpos/spl/spl_progs/MOD_5.xsm
+load --module 6 /home/kenil/expos/myexpos/spl/spl_progs/MOD_6.xsm
+load --module 7 /home/kenil/expos/myexpos/spl/spl_progs/MOD_7.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/even.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/odd.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/prime.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/gcd.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/sort.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/l_list.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/sample.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/shell.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/ll.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/oddev.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/child1.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/child2.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/parent.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/pid.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/shell2.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/sem.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/sem_test.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/merge.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/merge2.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/sem2.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/f_test.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/f_read.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/f_write.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/f_write2.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/readsem.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/writesem.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/f_sem.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/pg_test1.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/pgm1.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/pg_test2.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/pgm2.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/commands/rm.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/commands/ls.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/commands/cp.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/commands/cat.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/commands/lu.xsm
+load --exec /home/kenil/expos/myexpos/expl/samples/commands/ru.xsm
+load --data /home/kenil/expos/myexpos/sample.dat
+load --data /home/kenil/expos/myexpos/sample2.dat
